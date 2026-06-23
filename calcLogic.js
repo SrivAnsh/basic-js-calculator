15bcacacca5d1d8c378ba0251be6ef27e3403644
SrivAnsh <anshaj.nitin.srivastava@gmail.com>
2026-06-23 03:28:03 +0100

    Implemented the multiplication and division logic Finished version 1 of calculator

--- calcLogic.js
+++ calcLogic.js
@@ -1,7 +1,71 @@
 const outputBox = document.querySelector("input");
 
 function evalMulDiv(s) {
+    let sign = 1;
+    let num1 = null;
+    let num2 = null;
+    let marker = false;
+    let num1Started = false;
+    let num2Started = false;
+    let interval = [0,0];
 
+    for (let i=0; i<s.length; i++) {
+        if (marker===false) {
+            if (!("+-/*".includes(s[i]))) {
+                if (num1===null) {
+                    num1 = 0;
+                }
+                if (num1Started===false) {
+                    interval[0] = i;
+                    num1Started = true;
+                }
+                num1 = Number(s[i]) + num1 * 10;
+            } else if ("+-".includes(s[i])) {
+                num1 = null;
+                num1Started = true;
+                interval[0] = i;
+                sign = (s[i]==="+") ? 1 : -1;
+            } else if ("*/".includes(s[i])) {
+                if (num1===null) {
+                    return "ERROR"
+                }
+                marker = true;
+                num1 = sign*num1;
+                sign = 1;
+                op = (s[i]==="*") ? (x,y) => x*y : (x,y) => Math.floor(x/y);
+            } else {
+                return "ERROR"
+            }
+        } else {
+            if (!("+-*/".includes(s[i]))) {
+                if (num2===null) {
+                    num2 = 0;
+                }
+                num2 = Number(s[i]) + num2 * 10;
+                num2Started = true;
+            } else if (("+-".includes(s[i])) && (num2Started===false)) {
+                sign = (s[i]==="+") ? 1 : -1;
+            } else if (("+-*/".includes(s[i])) && (num2Started===true)) {
+                interval[1] = i;
+                break
+            } else {
+                return "ERROR"
+            }
+        }
+    }
+
+    if ((num1===null) || (num2===null)) {
+        return "ERROR"
+    }
+    console.log(s.slice(0,interval[0]) + "+" + String(op(num1,num2)) + s.slice(interval[1]))
+
+    console.log(num1,num2)
+
+    if ((num2Started===true) && (interval[1]===0)) {
+        return s.slice(0,interval[0]) + "+" + String(op(num1,num2))
+    } else {
+        return s.slice(0,interval[0]) + "+" + String(op(num1,num2)) + s.slice(interval[1])
+    }
 }
 
 function evaluate(s) {
@@ -11,9 +75,11 @@ function evaluate(s) {
     let total = 0;
     let prevSign = false;
 
-    while ((s.includes("*")) || (s.includes("*"))) {
+    while ((s.includes("*")) || (s.includes("/"))) {
         s = evalMulDiv(s);
     }
+
+
 
     for (let i=0; i<s.length; i++) {
         if ("0123456789".includes(s[i])) {
